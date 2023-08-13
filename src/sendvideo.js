7425c0581622d35e3e1be3f60b5a4406ebfa0353
shado <shadowstakeus28@gmail.com>
2023-08-13 13:29:51 +0300

    Initial commit

---
+++ src/sendvideo.js
@@ -0,0 +1,20 @@
+// CONNECT TO DB
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+async function main() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+        // Your GridFS operations will go here
+        const db = client.db("myTestDatabase")
+    } finally {
+        await client.close();
+        console.log("Disconnected from MongoDB");
+    }
+}
+
+main().catch(console.error);
